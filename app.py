b7950b32714568ebb5db4fafe199e4e11d144d3b
Muhammad Abbasi <muhammad.abbasi.team@gmail.com>
2021-05-20 22:23:23 +0500

    app.py updated

--- app.py
+++ app.py
@@ -23,15 +23,15 @@ app = Flask(__name__)
 MODEL_PATH = 'models/model_resnet.h5'
 
 # Load your trained model
-# model = load_model(MODEL_PATH)
-# model._make_predict_function()          # Necessary
-# print('Model loaded. Start serving...')
+model = load_model(MODEL_PATH)
+model._make_predict_function()          # Necessary
+print('Model loaded. Start serving...')
 
 # You can also use pretrained model from Keras
 # Check https://keras.io/applications/
-from keras.applications.resnet50 import ResNet50
-model = ResNet50(weights='imagenet')
-model.save('model_resnet.h5')
+# from keras.applications.resnet50 import ResNet50
+# model = ResNet50(weights='imagenet')
+# model.save('model_resnet.h5')
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
